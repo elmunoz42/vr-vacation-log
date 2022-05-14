aaa4a9b12bb3295d35bc9e92f8938ab0b470c396
cmk-fountain-city <carlos@fountain-city.com>
2022-05-14 16:32:22 -0300

    create app.js in new source folder and get it to work.

--- index.vr.js
+++ index.vr.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import {
   AppRegistry,
-  View,
-  Pano,
-  Text,
-  asset
+  View
 } from 'react-vr';
-// import { App } from './src/App';
+import App from './src/App';
 
 export default class sitio extends React.Component {
   render() {
     return (
       <View>
-       {/* <App /> */}
-        <Pano source={asset('salvador-panorama2.jpg')} />
-        <Text
-          style={{
-            backgroundColor: 'rgba(190,232,102,0.6)',
-            fontSize: 0.6,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{ translate: [0, 0, -3] }],
-          }}>
-          Mother's day - Salvador, Bahia, May 2022
-        </Text>
+       <App />
       </View>
     );
   }

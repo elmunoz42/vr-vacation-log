7cf3f2563b2a3af826f986ff9f9a95c4c228b98e
cmk-fountain-city <carlos@fountain-city.com>
2022-05-15 22:18:23 -0300

    add buttons and images for proof of concept.

--- README.md
+++ README.md
@@ -5,20 +5,13 @@ https://www.linkedin.com/learning/learning-react-vr/welcome?autoplay=true&resume
 
 ## Introduction:
 
-1. To create a 360 image use the Pano element and simply call the jpeg file that you added to the static_assets folder.
-2. You can create Primitive 3d objects using the Sphere, Box, Cylinder and other 3d elements from the react vr library. TODO this is not working for me though...
-
-## Other resources:
-
-https://www.inapps.net/how-to-make-react-vr-apps-in-2022
-
-## Talk by Instagram and Oculus devs:
+Talk by Instagram and Oculus devs:
 
 https://www.youtube.com/watch?v=t-645fMmuD0
 
-### Summary:
-- React VR has been renamed React 360 !!!
-- It is based on React because React is...
+- React VR has been renamed React 360 some documentation pages no longer exists, it seems that this project is a bit amorphous or in transition at this moment.
+
+- React VR is based on React because React is...
 * Declarative - UI creation is simplified by using declarations of what you want to see.
 * Component Based - UI elements are components that are developed in components.
 
@@ -27,6 +20,23 @@ https://www.youtube.com/watch?v=t-645fMmuD0
 * React Native has native modules for the given hardware
 
 - React VR is built on top of React Native
+
+- If you inspect the browser elements you'll see that React VR uses a <canvas> html element to render the Virtual reality experience. I haven't found the chrome development tools to be particularly useful in inpspecting the VR scenes for that reason. Learning about canvas might be a worthwile goal in understanding how this framework is put together.
+
+## Cook Book:
+
+1. To create a 360 image use the Pano element and simply call the jpeg file that you added to the static_assets folder.
+2. You can create Primitive 3d objects using the Sphere, Box, Cylinder and other 3d elements from the react vr library. (TODO this is not working for me)
+3. Using VrButton and react state you can change the background (or other display features) of the app. This can create a navigation experience.
+
+## Other resources:
+
+2022 React VR Article
+https://www.inapps.net/how-to-make-react-vr-apps-in-2022
+
+Quick Introdutory Canvas tutorial
+https://youtu.be/3GqUM4mEYKA
+
 
 
 
--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import {
     AmbientLight,
     VrButton
 } from 'react-vr';
-// import Primitives from './Primitives';
     
 
 export default class App extends React.Component {
@@ -25,18 +24,6 @@ export default class App extends React.Component {
     }
     loadScene = () => {
         return <Pano source={asset(this.state.background)} />;
-        // if (value === 1) {
-        //     return 'salvador-panorama2.jpg';
-        // } else if (value === 2) {
-        //     return 'salvador-panorama.jpg';
-        // } else if (value === 3) {
-        //     return 'salvador-panorama2.jpg';
-        // } else if (value === 4) {   
-        //     return 'salvador-panorama.jpg';
-        // } else {
-        //     return 'salvador-panorama2.jpg';
-        // }
-        
     }
     render() {
         return (
@@ -44,48 +31,54 @@ export default class App extends React.Component {
                 <AmbientLight intensity={2.5} />
                 <VrButton onClick={() => this.changeScene('salvador-panorama2.jpg')}><Text style={{
                     backgroundColor: 'rgba(190,232,102,0.6)',
-                    fontSize: 0.6,
+                    fontSize: 0.2,
                     fontWeight: '400',
                     layoutOrigin: [0.5, 0.5],
                     paddingLeft: 0.2,
                     paddingRight: 0.2,
+                    marginBottom: 0.2,
                     textAlign: 'center',
                     textAlignVertical: 'center',
                     transform: [{ translate: [0, 0, -3] }],
-                }}>Rio Vermelhow</Text></VrButton>
-                <VrButton onClick={() => this.changeScene('chess-world.jpg')}><Text style={{
+                }}>Rio Vermelho</Text></VrButton>
+                <VrButton onClick={() => this.changeScene('itapoa.jpg')}><Text style={{
                     backgroundColor: 'rgba(190,232,102,0.6)',
-                    fontSize: 0.6,
+                    fontSize: 0.2,
                     fontWeight: '400',
                     layoutOrigin: [0.5, 0.5],
                     paddingLeft: 0.2,
                     paddingRight: 0.2,
+                    marginBottom: 0.2,
                     textAlign: 'center',
                     textAlignVertical: 'center',
                     transform: [{ translate: [0, 0, -3] }],
-                }}>Chess</Text></VrButton>
-                {/* <VrButton onClick={this.changeScene('salvador-panorama.jpg')}><Text>Praia do Forte</Text></VrButton> */}
-                {/* <VrButton onClick={this.setScene(3)}><Text>Itapoa</Text></VrButton>
-                <VrButton onClick={this.setScene(4)}><Text>Iberastar</Text> </VrButton>
-                <VrButton onClick={this.setScene(5)}><Text>Rio Vermelho</Text> </VrButton> */}
-                {/* <Pano source={asset('salvador-panorama2.jpg')} /> */}
+                }}>Itapoa</Text></VrButton>
+                <VrButton onClick={() => this.changeScene('pelourinho.jpg')}><Text style={{
+                    backgroundColor: 'rgba(190,232,102,0.6)',
+                    fontSize: 0.2,
+                    fontWeight: '400',
+                    layoutOrigin: [0.5, 0.5],
+                    paddingLeft: 0.2,
+                    paddingRight: 0.2,
+                    marginBottom: 0.2,
+                    textAlign: 'center',
+                    textAlignVertical: 'center',
+                    transform: [{ translate: [0, 0, -3] }],
+                }}>Pelourinho</Text></VrButton>
+                <VrButton onClick={() => this.changeScene('rio-vermelho-watercolor.jpg')}><Text style={{
+                    backgroundColor: 'rgba(190,232,102,0.6)',
+                    fontSize: 0.2,
+                    fontWeight: '400',
+                    layoutOrigin: [0.5, 0.5],
+                    paddingLeft: 0.2,
+                    paddingRight: 0.2,
+                    marginBottom: 0.2,
+                    textAlign: 'center',
+                    textAlignVertical: 'center',
+                    transform: [{ translate: [0, 0, -3] }],
+                }}>Salvador Watercolor</Text></VrButton>
+                
                 {this.loadScene()}
-                {/* <Pano source={asset(this.loadScene(5))} /> */}
-                {/* <Text
-                    style={{
-                        backgroundColor: 'rgba(190,232,102,0.6)',
-                        fontSize: 0.6,
-                        fontWeight: '400',
-                        layoutOrigin: [0.5, 0.5],
-                        paddingLeft: 0.2,
-                        paddingRight: 0.2,
-                        textAlign: 'center',
-                        textAlignVertical: 'center',
-                        transform: [{ translate: [0, 0, -3] }],
-                    }}>
-                    Mother's day - Salvador, Bahia, May 2022
-                </Text> */}
-                {/* <Primitives /> */}
             </View>
         );
     }
